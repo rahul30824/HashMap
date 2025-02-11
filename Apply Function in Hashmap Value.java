90cd311950ea16f0d7357315e2e2c8da88e4156c
rahul30824 <rahulr684.sse@saveetha.com>
2025-02-11 19:33:55 +0530

    Apply Function in Hashmap Value.java

--- Apply Function in Hashmap Value.java
+++ Apply Function in Hashmap Value.java
@@ -13,18 +13,19 @@ class Main {
 
     public static void main(String[] args) {
         int[] array = {20, 5, 11, 28, 19, 108};
-    HashMap<Integer,Integer>fmap=new HashMap<>();
+    HashMap<Integer,Integer>fmap=new HashMap<>(); //Step 1 :Create A Hashmap//
     for(int num:array)
     {
-        fmap.put(num,factors(num));
+        fmap.put(num,factors(num));           //Step2 :Add Element into it//
     }
-List<Map.Entry<Integer,Integer>>map=new ArrayList<>(fmap.entrySet());
-map.sort((a, b) ->  Integer.compare(b.getValue(),a.getValue()));
+List<Map.Entry<Integer,Integer>>map=new ArrayList<>(fmap.entrySet()); //Insert both numbers and factors in the Map//
+map.sort((a, b) ->  Integer.compare(b.getValue(),a.getValue()));  //Compare using Lamda Expresion//
 
 for(Map.Entry<Integer,Integer>entry:fmap)
 {
-    System.out.print(entry.getKey());
+    System.out.print(entry.getKey());         //Print accordinh to that//
 }
     
 }
 }
+
